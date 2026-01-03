f2f09b878fb105239dd8cb3c37bcbd47df5dd6c2
Miriam Ali <ali.miriam.ohunene@gmail.com>
2026-01-03 09:46:13 -0600

    dded.env to .gitignore and saved env file

---
+++ videos_stats.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+
+API_KEY= os.getenv("API_KEY")
+CHANNEL_HANDLE="MrBeast"
+# response=requests.get(url)
+# print(response)
+
+def get_playlist_id():
+    try:
+        url= f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
+        response=requests.get(url)
+        response.raise_for_status
+        data= response.json()
+        channel_items= data["items"][0]
+        channel_playlistid= channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
+        print(channel_playlistid)
+        return channel_playlistid
+    except requests.exceptions.RequestException as e:
+        raise e
+if __name__== "__main__":
+    get_playlist_id()
